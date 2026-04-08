700df926bc0025be2f0b1188c53468bd9c9ecd91
Umme-Habiba123 <habibaislammim@gmail.com>
2026-04-08 22:59:46 +0600

    edited footer

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ Floka</a>
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li><a>Home</a></li>
         <li>
-          <a>Pages</a>
+          <a>Pagesssss</a>
           
         </li>
         <li><a>Portfolio</a></li>
